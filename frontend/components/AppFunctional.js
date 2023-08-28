7c3ed9ef0cc06d6858666aa0fb2a48db8c0d38ab
Jacob Lang <jacoblang72@comcast.net>
2023-08-28 15:07:54 -0400

    wip

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,49 +1,76 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 // Suggested initial states
 const initialMessage = "";
 const initialEmail = 'jacob@gmail.com'
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
+
+async function fetchData(aMessage) {
+  const res = await axios.post("http://localhost:9000/api/result", aMessage);
+  try {
+    return res;
+  } catch {
+    console.error("This Did Not Work");
+  }
+}
+
+
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
-  const [message,setMessage] = useState(initialMessage);
-  const [email,setEmail] = useState(initialEmail);
-  const [steps,setSteps] = useState(initialSteps);
-  const [index,setIndex] = useState(initialIndex); 
-  const [array,setArray] = useState([null,null,null,null,4,null,null,null,null]);
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [index1, setIndex1] = useState(initialIndex);
+  const [array, setArray] = useState([0, 1, 2, 3, 4, 5,6, 7, 8]);
+  const [coords, setCoords] = useState("2,2");
 
-  function getXY() {
+
+  const coordTemplate = [
+    [1, 1], [1, 2], [1, 3],
+    [2, 1], [2, 2], [2, 3],
+    [3, 1], [3, 2], [3, 3]
+  ];
+
+  function getXY(i) {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-    
-  }
-
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+    let [x,y] = coordTemplate[index1];
+    setCoords(`${x},${y}`);
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
-
+    setCoords("2,2");
+    setIndex1(initialIndex);
+    setSteps(initialSteps);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    if (direction === "RIGHT") {
+      return index1 + 1;
+    }
+    if (direction === "LEFT") {
+      return index1 - 1;
+    }
+    if (direction === "UP") {
+      return index1 - 3;
+    } 
+    if (direction === "DOWN") {
+      return index1 + 3;
+    }
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
-   if (evt.target.textConent = /left/i) {
-
-   }
+    getXY(index1);
   }
 
   function onChange(evt) {
@@ -57,14 +84,14 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
+        <h3 id="coordinates">Coordinate : ({coords})</h3>
         <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         {
-         array.map(idx => (
-            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
-              {idx === index ? 'B' : null}
+          [0,1,2,3,4,5,6,7,8].map((idx) => (
+            <div key={idx} className={`square${idx === index1 ? ' active' : ''}`}>
+              {idx === index1 ? 'B' : null}
             </div>
           ))
         }
@@ -73,11 +100,11 @@ export default function AppFunctional(props) {
         <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button onClick = {move} id="left">LEFT</button>
-        <button onClick = {move} id="up">UP</button>
-        <button onClick = {move} id="right">RIGHT</button>
-        <button onClick = {move} id="down">DOWN</button>
-        <button onClick = {move} id="reset">reset</button>
+        <button onClick={move} id="left">LEFT</button>
+        <button onClick={move} id="up">UP</button>
+        <button onClick={move} id="right">RIGHT</button>
+        <button onClick={move} id="down">DOWN</button>
+        <button onClick={reset} id="reset">reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
@@ -86,3 +113,18 @@ export default function AppFunctional(props) {
     </div>
   )
 }
+
+
+
+
+// if (evt.target.textContent === "LEFT") {
+//   let xy = coordTemplate.at(index1);
+//   let [a,b] = xy;
+//   if (a >= xy[0] &&  b > 0) {
+//     b -= 1;
+//     setIndex1(()=> index1 - 1)
+//     setCoords(`${a},${b}`)
+//     setSteps(()=> steps + 1);
+//   } else {
+//     setMessage("You can't go left")
+//   }
